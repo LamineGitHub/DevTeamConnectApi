2af4dbe571264c46fd981e1bfdc811e4bd18167b
Lamine Diallo <bah07565@gmail.com>
2024-02-01 01:37:52 +0000

    feat: Add controllers

---
+++ app/Http/Controllers/EmployerController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\EmployerFormRequest;
+use App\Http\Resources\EmployerResource;
+use App\Models\Employer;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class EmployerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        return EmployerResource::collection(Employer::with('service')->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(EmployerFormRequest $request): EmployerResource
+    {
+        return new EmployerResource(Employer::create($request->all()));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Employer $employer): EmployerResource
+    {
+        return new EmployerResource($employer);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(EmployerFormRequest $request, Employer $employer): EmployerResource
+    {
+        $employer->update($request->all());
+
+        return new EmployerResource($employer);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Employer $employer): JsonResponse
+    {
+        $employer->delete();
+
+        return response()->json('Employé supprimé avec succès');
+    }
+}
---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ServiceFormRequest;
+use App\Http\Resources\ServiceResource;
+use App\Models\Service;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        return ServiceResource::collection(Service::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ServiceFormRequest $request): ServiceResource
+    {
+        return new ServiceResource(Service::create($request->all()));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Service $service): ServiceResource
+    {
+        return new ServiceResource($service);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ServiceFormRequest $request, Service $service): ServiceResource
+    {
+        $service->update($request->all());
+
+        return new ServiceResource($service);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Service $service): JsonResponse
+    {
+        $service->delete();
+
+        return response()->json('Service supprimé avec succès');
+    }
+}
