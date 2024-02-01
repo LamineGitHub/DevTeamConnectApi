e10350c0d379dd62cb26ed668b77b5f1c57ed77e
Lamine Diallo <bah07565@gmail.com>
2024-02-01 01:28:16 +0000

    feat: Add relationships and fillable to models

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Employer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'matricule',
+        'prenom',
+        'nom',
+        'tel',
+        'email',
+        'salaire',
+        'dateNaiss',
+        'service_id',
+    ];
+
+    public function service(): BelongsTo
+    {
+        return $this->belongsTo(Service::class);
+    }
 }
--- app/Models/Service.php
+++ app/Models/Service.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Service extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'libelle',
+    ];
+
+    public function employers(): HasMany
+    {
+        return $this->hasMany(Employer::class);
+    }
 }
